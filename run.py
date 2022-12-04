1eea00ddeea656f33ec46ccf1c058e31577c1bbe
Grainne Donegan <grainnejdonegan@gmail.com>
2022-12-04 16:16:05 +0000

    create a contact page linking to the home and about pages

--- run.py
+++ run.py
@@ -7,6 +7,10 @@ app = Flask(__name__) # first argument is the name of the application's module
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
